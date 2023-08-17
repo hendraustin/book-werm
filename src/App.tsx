1af4dd8342e0e20377d33d4641762b11f33fcf4c
Austin Hendricks <hendraustin@gmail.com>
2023-08-17 12:10:02 -0700

    Format with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import BookInformation from './components/bookInformation';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import BookInformation from "./components/bookInformation";
+import "./App.css";
 
 function App() {
   const [isbn, setIsbn] = useState("Haven't searched");
   const [title, setTitle] = useState("Nothing here");
-  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,15 +15,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <div>
-          <BookInformation 
-            isbn={isbn}
-            setIsbn={setIsbn}
-            setTitle={setTitle}
-          />
+          <BookInformation isbn={isbn} setIsbn={setIsbn} setTitle={setTitle} />
           <div>
-            <p>
-              Title: {title}
-            </p>
+            <p>Title: {title}</p>
           </div>
         </div>
       </header>
--- src/components/bookInformation.tsx
+++ src/components/bookInformation.tsx
@@ -1,48 +1,50 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 
 type Props = {
   isbn: string;
   setIsbn: (val: string) => void;
   setTitle: (val: string) => void;
-}
+};
 
-const BookInformation: React.FC<Props> = ({isbn, setIsbn, setTitle}) => {
-    const getISBNInformation = () => {
-
-      let isbnNoHypens = isbn.replace("-", "");
-      axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnNoHypens}`)
+const BookInformation: React.FC<Props> = ({ isbn, setIsbn, setTitle }) => {
+  const getISBNInformation = () => {
+    let isbnNoHypens = isbn.replace("-", "");
+    axios
+      .get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbnNoHypens}`)
       .then((response) => {
-        setIsbn(isbnNoHypens)
-        setTitle(response.data.items[0].volumeInfo.title)
-      }).catch((error) => {
-        console.log(error)
-        setIsbn("Error reading ISBN")
+        setIsbn(isbnNoHypens);
+        setTitle(response.data.items[0].volumeInfo.title);
       })
-    }
+      .catch((error) => {
+        console.log(error);
+        setIsbn("Error reading ISBN");
+      });
+  };
 
-    const handleChange = (isbn: string) => {
-      setIsbn(isbn)
-    }
+  const handleChange = (isbn: string) => {
+    setIsbn(isbn);
+  };
 
-    return (
-      <form onSubmit={(event) => {
-        event.preventDefault()
-        getISBNInformation()
-        }
-      }>
-        <label>
-          ISBN:
-          <input type='text' name='isbn'
-            onChange={(e) => {
-                handleChange(e.target.value)
-              }
-            }
-          />
-        </label>
-        <input type='submit' value="Submit"/>
-      </form>
-    )
-}
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        getISBNInformation();
+      }}
+    >
+      <label>
+        ISBN:
+        <input
+          type="text"
+          onChange={(e) => {
+            handleChange(e.target.value);
+          }}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
 
-export default BookInformation
+export default BookInformation;
