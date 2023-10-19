6c10b905937d5a3da7b273144259deb13e0b9014
Austin Hendricks <hendraustin@gmail.com>
2023-10-19 10:25:13 -0700

    Update metadataArray count for exporting to csv

--- src/components/bookTitleItem.tsx
+++ src/components/bookTitleItem.tsx
@@ -1,19 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
+import { Metadata } from "../App";
+
 type Props = {
   bookTitle: string;
-  list: any[];
-  setList: (val: any[]) => void;
+  metadataArray: Metadata[];
+  setMetadataArray: (val: Metadata[]) => void;
 };
 
-function BookTitleItem({ bookTitle, list, setList }: Props) {
+function BookTitleItem({ bookTitle, metadataArray, setMetadataArray }: Props) {
   const [bookCount, setBookCount] = useState(1);
 
   const removeBookFromList = (bookTitle: string) => {
-    setList(list.filter((element) => element !== bookTitle));
+    setMetadataArray(metadataArray.filter((element) => element.title !== bookTitle));
   };
+
+  const updateQuantity = (bookTitle: string, newQuantity: number) => {
+    let updatedArray = metadataArray.map((element) => {
+      if (element.title === bookTitle) {
+        return { ...element, quantity: newQuantity.toString() };
+      }
+      return element;
+    });
+    setMetadataArray(updatedArray);
+  };
+
   return (
     <li>
       <FontAwesomeIcon
@@ -25,13 +38,19 @@ function BookTitleItem({ bookTitle, list, setList }: Props) {
       <FontAwesomeIcon
         className="bn-book-count"
         icon={faMinus}
-        onClick={() => setBookCount(Math.max(bookCount - 1, 1))}
+        onClick={() => {
+          updateQuantity(bookTitle, Math.max(bookCount - 1, 1));
+          setBookCount(Math.max(bookCount - 1, 1));
+        }}
       />
       {bookCount}
       <FontAwesomeIcon
         className="bn-book-count"
         icon={faPlus}
-        onClick={() => setBookCount(bookCount + 1)}
+        onClick={() => {
+          updateQuantity(bookTitle, Math.max(bookCount + 1, 1));
+          setBookCount(bookCount + 1);
+        }}
       />
     </li>
   );
