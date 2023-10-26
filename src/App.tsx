900edcec2a88952aeeaeb18f7fa8d0ac5844b191
Austin Hendricks <hendraustin@gmail.com>
2023-10-26 16:46:53 -0700

    Redux Toolkit Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,16 @@
-import { useState } from "react";
-
 import BookInformation from "./components/bookInformation";
 import BookTitleList from "./components/bookTitleList";
 import "./App.css";
 
-export type Metadata = {
-  author: string;
-  title: string;
-  quantity: string;
-};
-
 function App() {
-  const [isbn, setIsbn] = useState("");
-  const [metadataArray, setMetadataArray] = useState<Metadata[]>([]);
-
   return (
     <div className="App">
       <div className="app-column">
         <h1>Book Werm</h1>
         <img src={require("./book-werm.png")} className="book-werm-logo" alt="Book Werm logo" />
         <div>
-          <BookInformation
-            isbn={isbn}
-            setIsbn={setIsbn}
-            metadataArray={metadataArray}
-            setMetadataArray={setMetadataArray}
-          />
-          <BookTitleList metadataArray={metadataArray} setMetadataArray={setMetadataArray} />
+          <BookInformation />
+          <BookTitleList />
         </div>
       </div>
     </div>
