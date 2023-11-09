c492d5ae7062a74e04ba27c786294d90af2dcf82
Austin Hendricks <hendraustin@gmail.com>
2023-11-09 12:41:57 -0800

    Test decrementQuantity for bookSlice

--- src/features/book/bookSlice.test.ts
+++ src/features/book/bookSlice.test.ts
@@ -1,4 +1,4 @@
-import reducer, { BookMetadata, addMetadata, removeMetadata, incrementQuantity } from './bookSlice'
+import reducer, { BookMetadata, addMetadata, removeMetadata, incrementQuantity, decrementQuantity } from './bookSlice'
 
 test('should handle metadata being added to an empty list', () => {
     const previousState: BookMetadata[] = []
@@ -54,6 +54,30 @@ test('should do nothing if title not in existing list', () => {
     const previousState: BookMetadata[] = [{author: "Test Author A", title: "Test Title A", quantity: 1}]
 
     expect(reducer(previousState, incrementQuantity("Test Title B"))).toEqual([
+        {author: "Test Author A", title: "Test Title A", quantity: 1},
+    ])
+})
+
+test('should handle metadata quantity being decremented if title in existing list', () => {
+    const previousState: BookMetadata[] = [{author: "Test Author A", title: "Test Title A", quantity: 2}]
+
+    expect(reducer(previousState, decrementQuantity("Test Title A"))).toEqual([
+        {author: "Test Author A", title: "Test Title A", quantity: 1},
+    ])
+})
+
+test('should handle metadata not decrementing if value is one in existing list', () => {
+    const previousState: BookMetadata[] = [{author: "Test Author A", title: "Test Title A", quantity: 1}]
+
+    expect(reducer(previousState, decrementQuantity("Test Title A"))).toEqual([
+        {author: "Test Author A", title: "Test Title A", quantity: 1},
+    ])
+})
+
+test('should do nothing if title not in existing list', () => {
+    const previousState: BookMetadata[] = [{author: "Test Author A", title: "Test Title A", quantity: 1}]
+
+    expect(reducer(previousState, decrementQuantity("Test Title B"))).toEqual([
         {author: "Test Author A", title: "Test Title A", quantity: 1},
     ])
 })
